48587936c12ecacc1736e361a45eb4db38888630
Skrit-ex <skritex0@gmail.com>
2022-07-04 00:52:21 +0300

    added

---
+++ src/HomeWork/Book.java
@@ -0,0 +1,37 @@
+package HomeWork;
+
+import java.util.Objects;
+
+public class Book implements PrintAble{
+    private String name;
+
+    public Book(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(name, book.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public void print() {
+
+    }
+}
---
+++ src/HomeWork/PrintAble.java
@@ -0,0 +1,5 @@
+package HomeWork;
+
+public interface PrintAble {
+    void print();
+}
